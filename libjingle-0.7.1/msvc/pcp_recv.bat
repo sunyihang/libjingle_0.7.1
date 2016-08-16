cf79901f362d269ff90f4f79e5316272df52e385
hyunwkim <nnhope@hotmail.com>
2016-08-16 18:04:59 +0900

    Add pcp project

---
+++ libjingle-0.7.1/msvc/pcp_recv.bat
@@ -0,0 +1,3 @@
+.\bin\debug\pcp.exe --verbose --xmpp-host=YourHost --xmpp-port=5222 --xmpp-use-tls=false test1@YourHost test3@YourHost/pcp:test.bin test_new.bin
+
+pause()
---
+++ libjingle-0.7.1/msvc/pcp_send.bat
@@ -0,0 +1,3 @@
+.\bin\debug\pcp.exe --xmpp-host=YourHost --xmpp-port=5222 --xmpp-use-tls=false test1@YourHost .\test.txt test3@YourHost/pcp:test01.txt
+
+pause()
